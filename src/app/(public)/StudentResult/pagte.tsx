206d4eda3aa24f562a1d4c25e34df43ab613961b
ArafatSani10 <mdarafatrahmansani17@gmail.com>
2026-04-26 23:40:15 +0600

    chnaging url..

--- src/app/(public)/StudentResult/pagte.tsx
+++ src/app/(public)/StudentResult/pagte.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+// import React from 'react';
 
-const page = () => {
-    return (
-        <div>
-<            <StudentResult></StudentResult>></StudentResult>
-            </div>
-    );
-};
+// const page = () => {
+//     return (
+//         <div>
+//             {/* <StudentResult></StudentResult> */}
+//         </div>
+//     );
+// };
 
-export default page;
+// export default page;
